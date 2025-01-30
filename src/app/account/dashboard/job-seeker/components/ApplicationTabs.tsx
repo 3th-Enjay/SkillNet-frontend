6aaadcd64fd9e2367689aa0dba9e99b85393e9cc
3th-Enjay <enjayjerey@gmail.com>
2025-01-30 21:55:35 +0100

    COMPLETE

--- src/app/account/dashboard/job-seeker/components/ApplicationTabs.tsx
+++ src/app/account/dashboard/job-seeker/components/ApplicationTabs.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import ApplicationTabBox from "./ApplicationTabBox";
+import AllFilters from "./SideAllFilters";
+
 
 const ApplicationTab = () => {
-  return <div>No ApplicationTab yet</div>;
+  return ( <div className="mt-4 flex justify-between">
+    <ApplicationTabBox/>
+    <AllFilters/>
+  </div>
+  );
 };
 
 export default ApplicationTab;
